57d4a8a28fd428647e7b68e9459087b618932683
plaul <lmor@kea.dk>
2022-08-20 11:28:40 +0200

    Update SetupDevUsers.java
    Minor changes to default users

--- src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
+++ src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
@@ -36,10 +36,9 @@ public class SetupDevUsers implements ApplicationRunner {
         UserWithRoles user1 = new UserWithRoles("user1", passwordUsedByAll, "user1@a.dk");
         UserWithRoles user2 = new UserWithRoles("user2", passwordUsedByAll, "user2@a.dk");
         UserWithRoles user3 = new UserWithRoles("user3", passwordUsedByAll, "user3@a.dk");
-        UserWithRoles user4 = new UserWithRoles("user4", passwordUsedByAll, "user4@a.dk");
         user1.addRole(Role.USER);
         user1.addRole(Role.ADMIN);
-        user3.addRole(Role.USER);
+        user2.addRole(Role.USER);
         user3.addRole(Role.ADMIN);
         userWithRolesRepository.save(user1);
         userWithRolesRepository.save(user2);
