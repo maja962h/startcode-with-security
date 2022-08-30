f4439ce369ff10ec3ab0df3620e176514887ffb3
majamatboue <maja962h@stud.kea.dk>
2022-08-30 22:22:24 +0200

    removed tests

--- src/test/java/dat3/carsRus/repository/CarRepositoryTest.java
+++ src/test/java/dat3/carsRus/repository/CarRepositoryTest.java
@@ -25,7 +25,7 @@ class CarRepositoryTest {
 
     @Test
     void findCarByBrand() {
-       ;
+
     }
 
     @Test
--- src/test/java/dat3/carsRus/repository/MemberRepositoryTest.java
+++ src/test/java/dat3/carsRus/repository/MemberRepositoryTest.java
@@ -14,27 +14,16 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    private static String memberA;
-    private static String memberB;
 
     @BeforeAll
     public static void setUpData(@Autowired MemberRepository memberRepository){
-        Member m1 = new Member("Bob", "farlig", "bob@bob.com", "Bob", "Bobsen", "Enghavevej", "København", 2450, true, 3);
-        Member m2 = new Member("Janne", "sikker", "janne@janne.com", "Janne", "Jannesen", "Finsensvej", "Fredesriksberg", 2000, true, 2);
-        memberRepository.save(m1);
-        memberRepository.save(m2);
 
-        memberA = m1.getEmail();
-        memberB = m2.getEmail();
     }
 
 
     @Test
     public void testFindByEmail(){
-        Member found = memberRepository.findMemberByEmail("janne@janne.com");
 
-        assertEquals(memberB, found.getEmail());
-        assertEquals(memberA, "bob@bob.com");
 
     }
 
